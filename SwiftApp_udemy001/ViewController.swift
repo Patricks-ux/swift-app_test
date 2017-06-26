105b4379ae97792c955a9b5f642fde512612f09b
Patrick Schirvanian <apple@Patrick-Schirvanians-iMac.local>
2017-06-26 21:22:59 +0100

    this is my first commit

---
+++ SwiftApp_udemy001/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SwiftApp_udemy001
+//
+//  Created by Patrick Schirvanian on 24/06/2017.
+//  Copyright © 2017 uk.porden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLabel: UILabel!
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+       coolLabel.text = "Hello there!"
+        tapCount += 1
+        if tapCount >= 10 {
+          coolLabel.text = "you tapped the button 10 times"         
+        }
+    }
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        coolLabel.textColor = UIColor.white
+        self.view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
